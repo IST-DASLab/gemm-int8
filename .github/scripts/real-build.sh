8cf8f5836a619670f2df5acc0ca03aaa28ac1990
Rush Tabesh <soroushtabesh@gmail.com>
2025-03-06 16:34:52 +0100

    Migrate to pyproject.toml and modernize build configuration

--- .github/scripts/real-build.sh
+++ .github/scripts/real-build.sh
@@ -1,4 +1,5 @@
 # abort if error
 set -e
-pip3 install -r requirements-build.txt
+# pip3 install -r requirements-build.txt
+pip3 install .
 python3 setup.py bdist_wheel
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import torch.utils.cpp_extension as torch_cpp_ext
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import os
@@ -35,15 +35,10 @@ def remove_unwanted_pytorch_nvcc_flags():
 
 def get_cuda_arch_flags():
     return [
-        # '-gencode', 'arch=compute_75,code=sm_75',  # Turing
-        "-gencode",
-        "arch=compute_80,code=sm_80",  # Ampere
-        "-gencode",
-        "arch=compute_86,code=sm_86",  # Ampere
-        "-gencode",
-        "arch=compute_89,code=sm_89",  # Ada
-        "-gencode",
-        "arch=compute_90,code=sm_90",  # Hopper
+        "-gencode", "arch=compute_80,code=sm_80",  # Ampere
+        "-gencode", "arch=compute_86,code=sm_86",  # Ampere
+        "-gencode", "arch=compute_89,code=sm_89",  # Ada
+        "-gencode", "arch=compute_90,code=sm_90",  # Hopper
         "--expt-relaxed-constexpr",
     ]
 
@@ -60,18 +55,12 @@ def third_party_cmake():
     build_dir = HERE / "build"
     build_dir.mkdir(exist_ok=True)
 
-    retcode = subprocess.call(
-        [
-            cmake,
-            "-B",
-            str(build_dir),
-            "-S",
-            str(HERE),
-            "-G",
-            "Ninja",
-            # "-DCMAKE_BUILD_PARALLEL_LEVEL=8",  # Use 8 CPUs for parallel build
-        ]
-    )
+    retcode = subprocess.call([
+        cmake,
+        "-B", str(build_dir),
+        "-S", str(HERE),
+        "-G", "Ninja",
+    ])
     if retcode != 0:
         sys.stderr.write("Error: CMake configuration failed.\n")
         sys.exit(1)
@@ -92,63 +81,25 @@ class CustomBuildExtension(BuildExtension):
         super().run()
 
 
-install_requires = [
-    "torch>=2.0.0",
-]
-
-build_requires = [
-    "cmake>=3.18.0",
-    "ninja",
-]
-
-if __name__ == "__main__":
-    setup(
-        name="gemm_int8",
-        version="1.0.0",
-        author="Rush Tabesh",
-        author_email="soroushtabesh@gmail.com",
-        description="High-performance INT8 matrix multiplication CUDA extension for PyTorch",
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        url="https://github.com/IST-DASLab/gemm-int8",
-        project_urls={
-            "Bug Tracker": "https://github.com/IST-DASLab/gemm-int8/issues",
-            "Documentation": "https://github.com/IST-DASLab/gemm-int8#readme",
-        },
-        packages=find_packages(),
-        ext_modules=[
-            CUDAExtension(
-                name="gemm_int8.gemm_int8_CUDA",
-                sources=[
-                    "gemm_int8/kernels/bindings.cpp",
-                    "gemm_int8/kernels/gemm.cu",
-                ],
-                include_dirs=[
-                    os.path.join(setup_dir, "gemm_int8/kernels/include"),
-                    os.path.join(setup_dir, "cutlass/include"),
-                    os.path.join(setup_dir, "cutlass/tools/util/include"),
-                ],
-                extra_compile_args={
-                    "cxx": [],
-                    "nvcc": get_cuda_arch_flags(),
-                },
-            )
-        ],
-        cmdclass={"build_ext": CustomBuildExtension},
-        classifiers=[
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: POSIX :: Linux",
-            "Topic :: Scientific/Engineering :: Artificial Intelligence",
-            "Intended Audience :: Science/Research",
-            "Development Status :: 4 - Beta",
-        ],
-        python_requires=">=3.9",
-        install_requires=install_requires,
-        extras_require={
-            "build": build_requires,
-        },
-        zip_safe=False,  # Required for C extensions
-    )
+# The actual setup call, using the ext_modules for CUDA extensions
+setup(
+    ext_modules=[
+        CUDAExtension(
+            name="gemm_int8.gemm_int8_CUDA",
+            sources=[
+                "gemm_int8/kernels/bindings.cpp",
+                "gemm_int8/kernels/gemm.cu",
+            ],
+            include_dirs=[
+                os.path.join(setup_dir, "gemm_int8/kernels/include"),
+                os.path.join(setup_dir, "cutlass/include"),
+                os.path.join(setup_dir, "cutlass/tools/util/include"),
+            ],
+            extra_compile_args={
+                "cxx": [],
+                "nvcc": get_cuda_arch_flags(),
+            },
+        )
+    ],
+    cmdclass={"build_ext": CustomBuildExtension},
+)
